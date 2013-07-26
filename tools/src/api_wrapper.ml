4f1e74ea670747adff193ad2cd987e0f5f08f387
Simon Marc <marc.simon42@gmail.com>
2013-07-25 19:15:33 -0700

    [fix] improving import

--- tools/src/api_wrapper.ml
+++ tools/src/api_wrapper.ml
@@ -1,6 +1,7 @@
 open Config
 
 exception Incorrect_response
+exception Replay_request
 
 type image_conf = {
   base_url : string;
@@ -76,49 +77,71 @@ module Movie_api = struct
   } deriving (Json_ext, Bson_ext)
 end
 
-let call_nb = ref 1
-let last_call_time = ref None
+let build_url =
+  let call_nb = ref 1 in
+  let first_time = ref None in
+  let sleep_time = 1.3 in
+  let retry_after = ref 0 in
 
-let build_url ?(params=[]) config ~uri =
+  let rec build_url_in ?(params=[]) config ~uri =
+    incr(call_nb);
+    let call_nb = !call_nb in
 
-  incr(call_nb);
-  lwt _ =
-    match !last_call_time with
-      | Some (cn, f) when !call_nb - cn >= config.request_per_second ->
-        let t = Unix.time () in
-        (* Printf.printf "Sleeping for %f\n%!" (1.05 -. (t -. f)); *)
-        lwt _ = Lwt_unix.sleep (1.05 -. (t -. f)) in
-        last_call_time := None;
-        Lwt.return_unit
-      | _ -> Lwt.return_unit
+    lwt _ =
+      match !first_time with
+        | None ->
+          first_time := Some (Unix.gettimeofday ());
+          Lwt.return_unit
+        | Some t ->
+          let t_ = Unix.gettimeofday () in
+
+          let n = call_nb / config.request_per_second in
+          let waiting_time = (float_of_int n) *. sleep_time +. (float_of_int !retry_after) in
+
+          if (t_ -. t < waiting_time) then begin
+            Lwt_unix.sleep (waiting_time -. (t_ -. t))
+          end else Lwt.return_unit
+    in
+
+    let url = Printf.sprintf "%s%s?api_key=%s" config.mock_api uri config.api_key in
+    let headers = [
+      "Accept", "application/json"
+    ] in
+
+    let url =
+      List.fold_left (
+        fun s (k,v) ->
+          s ^ (Printf.sprintf "&%s=%s" k v)
+      ) url params
+    in
+
+    lwt s,h = Http.get_url ~headers url () in
+
+    if call_nb mod 30 = 0 && call_nb != 0 then Printf.printf "c: %d; t = %f\n%!" call_nb (Unix.gettimeofday ());
+
+    lwt s =
+      Ocsigen_http_frame.Http_header.(
+        match h.mode with
+          | Answer i when i = 200 || i = 304 -> Lwt.return s
+          | Answer i when i = 429 ->
+            let n = get_headers_value h (Http_headers.name "Retry-After") in
+            Printf.printf "retry-after : %s\n%!" n;
+            lwt _ = Lwt_unix.sleep (float_of_string n) in
+            lwt s = build_url_in ~params config ~uri in
+            retry_after := !retry_after + (((int_of_string n) + config.max_connections) / config.max_connections);
+            Lwt.return s
+          | Answer i when i = 404 ->
+            raise Not_found
+          | _ ->
+            Printf.printf "ir2: %d: %s\n%!" call_nb s;
+            raise Incorrect_response
+      )
+    in
+
+
+    Lwt.return s
   in
-
-  let url = Printf.sprintf "%s%s?api_key=%s" config.mock_api uri config.api_key in
-  let headers = [
-    "Accept", "application/json"
-  ] in
-
-  let url =
-    List.fold_left (
-      fun s (k,v) ->
-        s ^ (Printf.sprintf "&%s=%s" k v)
-    ) url params
-  in
-
-  lwt s,h = Http.get_url ~headers url () in
-
-  begin match !last_call_time with
-    | Some n -> ()
-    | None -> last_call_time := Some (!call_nb, Unix.time ())
-  end;
-
-  Ocsigen_http_frame.Http_header.(
-    match h.mode with
-      | Answer i when i = 200 || i = 304 -> ()
-      | _ -> raise Incorrect_response
-  );
-
-  Lwt.return s
+  build_url_in
 
 
 let fetch_moviedb_configuration config =
@@ -127,7 +150,7 @@ let fetch_moviedb_configuration config =
 
 let fetch_last_movie config =
   lwt s = build_url config ~uri:"/3/movie/latest" in
-  Lwt.return (Movie_api.Json_ext_movie.from_json (Json_ext.from_string s))
+   Lwt.return (Movie_api.Json_ext_movie.from_json (Json_ext.from_string s))
 
 
 let fetch_movie_str config uid =
@@ -154,26 +177,31 @@ let load_movies config mongodb =
             Lwt.return_unit
           | Some t ->
             let t_ = Unix.time () in
-            (* Printf.printf "Sleeping insert for: %f\n%!" (1.05 -. (t_ -. t)); *)
-            lwt _ = Lwt_unix.sleep (1.05 -. (t_ -. t)) in
-            last_call_time := None;
+            lwt _ = Lwt_unix.sleep (10. -. (t_ -. t)) in
+            last_call_time := Some (Unix.time ());
             Lwt.return_unit
       in
 
+      Printf.printf "inserting %d movies\n%!" (List.length !movie_poll);
+
       List.iter (
         fun s ->
-          let m = Movie_api.Json_ext_movie.from_json (Json_ext.from_string s) in
+          try
+            let m = Movie_api.Json_ext_movie.from_json (Json_ext.from_string s) in
           (* Printf.printf "Saving : %d\n%!" m.Movie_api.id; *)
-          Mongo.insert mongodb [ (Movie_api.Bson_utils_movie.to_bson m) ];
+            Mongo.insert mongodb [ (Movie_api.Bson_utils_movie.to_bson m) ];
+          with exn ->
+            Printf.printf "err insert: %s || on %s\n%!" (Printexc.to_string exn) s
       ) !movie_poll;
 
-      movie_poll := [] ;
-
+      (* check if there was any value in movie_poll *)
       let n =
         match !movie_poll with
           | h::_ -> 0
           | _ -> n + 1
       in
+
+      movie_poll := [] ;
 
       if n < 10 then
         insert_db_in n
@@ -188,7 +216,6 @@ let load_movies config mongodb =
   in
 
   let rec fetch_in uid =
-    Printf.printf "fetch : %d\n%!" uid;
     if uid = 0 then Lwt.return_unit
     else begin
       lwt _ =
